1e0c11e8c5c6c95dfd56fa00328f4b18023b0a53
PSGamezG <paulita.gorillaz@gmail.com>
2024-01-02 15:48:03 -0500

    feat (client/chat)

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Nav from "./Components/Navbar/Nav";
 import Description from "./Components/Description/Description";
 import Projects from "./Components/Projects/Projects";
+import Chat from "./Components/Chat/Chat";
 
 function App() {
   return (
     <>
       <Nav />
       <div className="app-container">
+        <Chat />
         <Description />
         <Projects />
       </div>
--- my-portfolio/src/Components/Projects/Projects.jsx
+++ my-portfolio/src/Components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
     <div className="projects-container">
       <h2>Projects</h2>
 
-      <p>Here you can find some of my work</p>
+      <p>Here you can find some of my git and deployed work</p>
 
       <a
         href="https://github.com/PSGamezG/PI-countries"
@@ -21,6 +21,14 @@ const Projects = () => {
         rel="noopener noreferrer"
       >
         <img src="/wp6445020.jpg" alt="Project 1" />
+      </a>
+
+      <a
+        href="https://healthplus-henry.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="/HealthPlus.png" alt="Project 1" />
       </a>
     </div>
   );
